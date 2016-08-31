052a09c1e9c50b1960cd52681100385a660ac595
steve simpson <stevesimpson4375@gmail.com>
2016-08-30 20:26:45 -0400

    Checking in a simple database.

---
+++ DemoDB/Sales/Stored Procedures/uspNewCustomer.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [Sales].[uspNewCustomer]
+@CustomerName NVARCHAR (40),
+@CustomerID INT OUTPUT
+AS
+BEGIN
+INSERT INTO [Sales].[Customer] (CustomerName) VALUES (@CustomerName);
+SET @CustomerID = SCOPE_IDENTITY();
+RETURN @@ERROR
+END
